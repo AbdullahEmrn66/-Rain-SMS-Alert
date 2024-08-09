45386417c5e85581fc4a315e33dc74d3496e96d1
Muhammad Abdullah <110246911+AbdullahEmrn66@users.noreply.github.com>
2024-08-09 12:31:35 +0500

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ parameters = {
     "appid": "318bc5e4f7e480e162084c81b244be74",
     "cnt": "4"
 }
-account_sid = os.environ["AC6d781e7d0f4175c23ec0c787cca451d1"]
-auth_token = os.environ["59ea997dcc80cfd33db3661a4f4bb992"]
+account_sid = os.environ[SID]
+auth_token = os.environ[AUTH]
 
 OWN_endpoint = "https://api.openweathermap.org/data/2.5/forecast"
 response = requests.get(OWN_endpoint, params=parameters)
@@ -23,7 +23,7 @@ for hour in data:
 if will_rain:
     client = Client(account_sid, auth_token)
     message = client.messages.create(
-        body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+        body="It will rain tomorrow, carry an umbrella",
         from_="+12513254331",
         to="+923035987180",
     )
